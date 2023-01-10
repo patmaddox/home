0c63dfc7713dfcb49b974d6ceae00edc97d6938d
Pat Maddox <pat@patmaddox.com>
2023-01-10 12:25:22 -0800

    Configure default org files

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,2 +1,9 @@
-; disable package.el - needed for straight.el
+;; disable package.el - needed for straight.el
 (setq package-enable-at-startup nil)
+
+;; org / agenda config
+(setq org-agenda-files
+      '("~/priv/org" "~/ratio/org"))
+
+(setq org-todo-keywords
+      '((sequence "TODO" "WAIT" "HOLD" "|" "DONE")))
