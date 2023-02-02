c67dfe90fa63b69d402b1535cf33349b69464b7e
Pat Maddox <pat@patmaddox.com>
2023-02-02 10:15:50 -0800

    Disable projectile for time being

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,6 +21,6 @@
 (require 'ol) ; org-mode links
 
 ;; projectile
-(straight-use-package 'projectile)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-(projectile-mode +1)
+;(straight-use-package 'projectile)
+;(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+;(projectile-mode +1)
