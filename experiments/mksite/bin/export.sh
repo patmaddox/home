e59b6e20b9d235334b054ed0ff111ec84237098a
Pat Maddox <pat@patmaddox.com>
2023-03-28 01:47:48 -0700

    mksite: Link to relative paths

--- experiments/mksite/bin/export.sh
+++ experiments/mksite/bin/export.sh
@@ -4,14 +4,19 @@ set -o pipefail
 
 getpath=$(dirname $0)/getoutpath.sh
 selectpath=$(dirname $0)/selectpath.sh
+relpath=$(dirname $0)/relpath.sh
 
 mypath=$($getpath ${1})
+mydir=$(dirname $mypath)
+if [ ! -d $mydir ]; then mkdir -p $mydir; fi
 
 ids=$(cat $1 | grep -o '(%[[:digit:]]*%)' | grep -o '[[:digit:]]*' | sort -u)
 replace="-e '/^---$/,/^---$/d'"
+
 for id in $ids; do
-    path=$($selectpath $id | sed -e 's|^out/||')
-    replace="${replace} -e 's;(%${id}%);(${path});g'"
+    path=$($selectpath $id)
+    therelpath=$($relpath $mypath $path)
+    replace="${replace} -e 's;(%${id}%);(${therelpath});g'"
 done
 
 eval "sed $replace" $1 | markdown -o $2
