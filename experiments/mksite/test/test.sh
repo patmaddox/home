6f6ecf3e047957b41c16dbcda62915a4d591befd
Pat Maddox <pat@patmaddox.com>
2023-03-28 23:46:20 -0700

    mksite: extract a test helper

--- experiments/mksite/test/test.sh
+++ experiments/mksite/test/test.sh
@@ -7,10 +7,7 @@ test_basic_head() {
 }
 
 test_basic_body() {
-    if [ ! -d $(atf_get_srcdir)/test_basic/bin ]; then mkdir $(atf_get_srcdir)/test_basic/bin; fi
-    cp $(atf_get_srcdir)/../Makefile $(atf_get_srcdir)/test_basic
-    cp $(atf_get_srcdir)/../bin/mksite.sh $(atf_get_srcdir)/test_basic/bin/
-    
+    copy_mksite test_basic
     atf_check -s exit:0 -o ignore -x "make -C $(atf_get_srcdir)/test_basic"
     atf_check -s exit:0 -o match:'<h1>Hello World</h1>' -x "cat $(atf_get_srcdir)/test_basic/out/index.html"
 }
@@ -22,10 +19,7 @@ test_no_id_head() {
 }
 
 test_no_id_body() {
-    if [ ! -d $(atf_get_srcdir)/test_no_id/bin ]; then mkdir $(atf_get_srcdir)/test_no_id/bin; fi
-    cp $(atf_get_srcdir)/../Makefile $(atf_get_srcdir)/test_no_id
-    cp $(atf_get_srcdir)/../bin/mksite.sh $(atf_get_srcdir)/test_no_id/bin/
-
+    copy_mksite test_no_id
     atf_check -s exit:1 -o ignore -e inline:'src/no-id.md has no id\n' -x "make -C $(atf_get_srcdir)/test_no_id"
     atf_check -x "test ! -f $(atf_get_srcdir)/test_no_id/out/no-id.html"
 }
@@ -34,4 +28,11 @@ test_no_id_body() {
 atf_init_test_cases() {
     atf_add_test_case test_basic
     atf_add_test_case test_no_id
+}
+
+## helpers
+copy_mksite() {
+    if [ ! -d $(atf_get_srcdir)/${1}/bin ]; then mkdir $(atf_get_srcdir)/${1}/bin; fi
+    cp $(atf_get_srcdir)/../Makefile $(atf_get_srcdir)/${1}
+    cp $(atf_get_srcdir)/../bin/mksite.sh $(atf_get_srcdir)/${1}/bin/
 }
